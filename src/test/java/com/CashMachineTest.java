9f1b80e5f44f14b2d6ad8aade18f35f331021c0b
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-14 23:59:26 +0300

    Incorrect JFrame

---
+++ src/test/java/com/CashMachineTest.java
@@ -0,0 +1,37 @@
+package com;
+
+import org.junit.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by coura on 09.04.2016.
+ */
+public class CashMachineTest {
+
+    @org.junit.Test
+    public void testShowBalance() throws Exception {
+
+    }
+
+    @Test
+    public void testInputMoney() throws Exception {
+
+    }
+
+    @Test
+    public void testTakeMoney() throws Exception {
+
+    }
+
+    @Test
+    public void testGiveMoney() throws Exception {
+
+    }
+
+    @Test
+    public void testIsInteger() throws Exception {
+
+    }
+}
